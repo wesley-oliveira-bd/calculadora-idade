86e6f364595d93ec2cb730647ce688ddbcec0bd6
Wesley de Oliveira <wesley.oliveirabd@gmail.com>
2023-03-29 17:39:02 -0300

    criado pagina html e primeiras linhas de js

---
+++ script.js
@@ -0,0 +1,13 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('resultado')
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('Erro. Verifique os dados e tente novamente.')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        res.innerHTML = idade
+    }
+}
