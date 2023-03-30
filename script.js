4bb9afbd81cfa88416b47196c0c270a7128e1686
Wesley de Oliveira <wesley.oliveirabd@gmail.com>
2023-03-29 21:50:10 -0300

    finalizado
    finalizado as configurações do javascript

--- script.js
+++ script.js
@@ -8,6 +8,36 @@ function verificar() {
     } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        res.innerHTML = idade
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked){
+            genero = 'Homem'
+            if(idade >= 0 && idade < 14){
+                img.setAttribute('src', 'imagens/bebe_masc.jpg') 
+            } else if (idade < 21){
+                img.setAttribute('src', 'imagens/jovem_masc.jpg') //jovem
+            } else if (idade < 60) {
+                img.setAttribute('src', 'imagens/adulto_masc.jpg') //adulto
+            } else {
+                img.setAttribute('src', 'imagens/idoso_masc.jpg') //idoso
+            }
+        } else if (fsex[1].checked){
+            genero = 'Mulher'
+            if(idade >= 0 && idade < 14){
+                img.setAttribute('src', 'imagens/bebe_fem.jpg') //criança
+            } else if (idade < 21){
+                img.setAttribute('src', 'imagens/jovem_fem.jpg') //jovem
+            } else if (idade < 60) {
+                img.setAttribute('src', 'imagens/adulto_fem.jpg') //adulto
+            } else {
+                img.setAttribute('src', 'imagens/idoso_fem.jpg')//idoso
+            }
+        }
+        img.style.borderRadius= "50%"
+        res.style.textAlign = "center"
+        res.innerHTML = `Detectamos um(a) ${genero} com ${idade} anos. `
+        res.appendChild(img)
     }
+
 }
